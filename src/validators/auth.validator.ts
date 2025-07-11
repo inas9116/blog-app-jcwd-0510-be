3e9bbdbb45100cca2a3b6f348d9c99c466cffaf4
inas9116 <inas9116@gmail.com>
2025-07-11 23:13:41 +0700

    feat: login

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,19 +1,30 @@
 import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
+const handleVallidation = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400).send({ message: errors.array()[0].msg });
+    return;
+  }
+
+  next();
+};
+
 export const validateRegister = [
   body("name").notEmpty().withMessage("name is required").isString(),
-  body("emaikl").notEmpty().withMessage("email is required").isEmail(),
+  body("email").notEmpty().withMessage("email is required").isEmail(),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 6 })
+    .withMessage("password must be at least 6 characters"),
+  handleVallidation,
+];
+
+export const validateLogin = [
+  body("email").notEmpty().withMessage("email is required").isEmail(),
   body("password").notEmpty().withMessage("password is required"),
-
-  (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.status(400).send({ message: errors.array()[0].msg });
-      return;
-    }
-
-    next();
-  },
+  handleVallidation,
 ];
