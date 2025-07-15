d814f038f3f5e84683674cae94981e1db332f80e
inas9116 <139898118+inas9116@users.noreply.github.com>
2025-07-16 00:47:58 +0700

    fix: forgot password (#4)

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -1,10 +1,26 @@
 import nodemailer from "nodemailer";
 import { GMAIL_APP_PASSWORD, GMAIL_EMAIL } from "../config";
 
+// export const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: GMAIL_EMAIL,
+//     pass: GMAIL_APP_PASSWORD,
+//   },
+// });
+
+
+
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // pakai TLS (STARTTLS)
   auth: {
     user: GMAIL_EMAIL,
     pass: GMAIL_APP_PASSWORD,
   },
+  tls: {
+    rejectUnauthorized: false, // opsional: hindari sertifikat error
+  },
+  connectionTimeout: 10000, // (10 detik timeout)
 });
